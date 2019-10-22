37fda5a252db4e4a00a546c432fb0dfd0fa58db2
Erik Maier <erikmaier80@gmail.com>
2019-10-22 21:44:07 +0200

    small update

--- Assets/NewBehaviour.cs
+++ Assets/NewBehaviour.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    public int lebensEnergie = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Die Figur hat noch " + lebensEnergie + " Lebensenergie" );
     }
 
     // Update is called once per frame
